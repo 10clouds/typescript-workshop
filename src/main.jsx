1030cc0896353663bb94c65d379d10364cd03a54
Adam Florczak <adam.florczak@me.com>
2017-05-10 20:37:18 +0200

    Webpack setup with typescript and jsx

---
+++ src/main.jsx
@@ -0,0 +1,5 @@
+const React = require('react');
+const ReactDOM = require('react-dom');
+
+const appNode = document.getElementById('app');
+ReactDOM.render(<h2>Hello world</h2>, appNode);
---
+++ webpack.config.js
@@ -0,0 +1,38 @@
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+const CleanWebpackPlugin = require('clean-webpack-plugin');
+const path = require('path');
+
+const distDir = path.join(__dirname, './dist');
+
+module.exports = {
+  entry: {
+    main: './src/main.jsx',
+  },
+  output: {
+    filename: '[name].[hash].js',
+    path: distDir,
+  },
+
+  resolve: {
+    extensions: ['.ts', '.tsx', '.js', '.jsx']
+  },
+
+  devtool: 'source-map',
+
+  module: {
+    loaders: [
+      {
+        test: /\.(tsx?|jsx?)$/,
+        loader: 'awesome-typescript-loader',
+        exclude: /node_modules/,
+      },
+    ],
+  },
+
+  plugins: [
+    new HtmlWebpackPlugin({
+      template: './src/index.html',
+    }),
+    new CleanWebpackPlugin([distDir]),
+  ],
+};
