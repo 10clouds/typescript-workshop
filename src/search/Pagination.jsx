8c0e38bc91c861983ad1eacfaae77a5d7139921c
Adam Florczak <adam.florczak@me.com>
2017-05-22 18:27:31 +0200

    Use default import for React dependencies

--- src/search/Pagination.jsx
+++ src/search/Pagination.jsx
@@ -1,4 +1,4 @@
-import * as React from 'react';
+import React from 'react';
 import { Component } from 'react';
 
 export function Pagination(props) {
--- src/search/TrackList.jsx
+++ src/search/TrackList.jsx
@@ -1,4 +1,4 @@
-import * as React from 'react';
+import React from 'react';
 
 import {Track} from './Track';
 import {SpotifyTrackData} from './trackSearch';
