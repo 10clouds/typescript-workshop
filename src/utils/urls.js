049e7f65cbe77f08720403ceffc9eaa8bbf58b4a
Adam Florczak <adam.florczak@me.com>
2017-05-22 19:11:28 +0200

    Unneeded jsx extension on api class

--- src/utils/urls.js
+++ src/utils/urls.js
@@ -1,14 +1,11 @@
 export function buildQueryString(params) {
-	return ( 
-		Object
-		.keys(params)
-		.map((key) => [key, params[key]])
-		.map((pair) => pair.map(encodeURIComponent))
-		.map((pair) => pair.join('='))
-		.join('&')
-	);
+  return Object.keys(params)
+    .map((key) => [key, params[key]])
+    .map((pair) => pair.map(encodeURIComponent))
+    .map((pair) => pair.join('='))
+    .join('&');
 }
 
 export function buildUrl(baseUrl, params) {
-	return `${baseUrl}?${buildQueryString(params)}`;
+  return `${baseUrl}?${buildQueryString(params)}`;
 }
