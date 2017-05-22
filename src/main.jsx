e0fd8e09c72db59bc4f4532613c447a580daee8b
Adam Florczak <adam.florczak@me.com>
2017-05-22 18:59:03 +0200

    Import instead of require

--- src/main.jsx
+++ src/main.jsx
@@ -2,7 +2,7 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import {App} from './App';
 
-require('./styles.css');
+import './styles.css';
 
 const appNode = document.getElementById('app');
 ReactDOM.render(<App />, appNode);
