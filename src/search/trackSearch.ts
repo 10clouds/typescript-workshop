d61038e7eaaa0e4a65edaa9d126dfbb7e9d3e3b1
Adam Florczak <adam.florczak@me.com>
2017-05-23 23:57:50 +0200

    Step 3 changes

--- src/search/trackSearch.ts
+++ src/search/trackSearch.ts
@@ -3,11 +3,52 @@ import {buildUrl} from '../utils/urls';
 const baseUrl = `https://spotify-proxy-workshop.herokuapp.com/search`;
 export type Direction = 1|-1;
 
+interface SpotifyObject {
+  id: string;
+  type: string;
+  name: string;
+}
+
+interface Image {
+  height: number;
+  width: number;
+  url: string;
+}
+
+interface Album extends SpotifyObject {
+  album_type: string;
+  images: Image[];
+}
+
+interface Track extends SpotifyObject {
+  preview_url: string;
+  album: Album;
+  artists: Artist[];
+}
+
+interface Artist extends SpotifyObject { }
+
+interface SpotifyPagination {
+  items: SpotifyObject[];
+  next: string;
+  previous: string;
+}
+
+interface Tracks extends SpotifyPagination {
+  items: Track[];
+}
+
+interface SearchData {
+  tracks: Tracks;
+}
+
+const baseUrl = `https://api.spotify.com/v1/search`;
+
 export class TrackSearch {
   previousUrl: string;
   nextUrl: string;
 
-  async search(query: string, direction?: Direction) {
+  async search(query: string, direction?: Direction): Promise<SearchData> {
     let url;
 
     if (direction) {
