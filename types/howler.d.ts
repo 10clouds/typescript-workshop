4b2d283f04dcddb6817826788fc235747ec2cf4e
Marek Skrajnowski <mskrajnowski@users.noreply.github.com>
2017-05-23 23:57:13 +0200

    7. c) howler module interfaces

--- types/howler.d.ts
+++ types/howler.d.ts
@@ -1,1 +1,29 @@
-declare module 'howler';
+declare module 'howler' {
+    export namespace Howl {
+        export type Format = 'mp3' | 'ogg';
+        export type State = 'unloaded' | 'loading' | 'loaded';
+
+        export interface Options {
+            src: string[];
+            autoplay?: boolean;
+            preload?: boolean;
+            format?: Format[];
+        }
+    }
+
+    export class Howl {
+        constructor(options: Howl.Options);
+
+        state(): Howl.State;
+        load(): void;
+        play(): void;
+        playing(): boolean;
+        stop(): void;
+    }
+
+    export interface HowlerGlobal {
+        volume(number): void;
+    }
+
+    export const Howler: HowlerGlobal;
+}
