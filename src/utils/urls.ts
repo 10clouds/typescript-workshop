be839004664d6bffb42dc4d8119dfdb656810858
Marek Skrajnowski <mskrajnowski@users.noreply.github.com>
2017-12-04 22:16:22 +0100

    Step 2: urls.ts type annotations

--- src/utils/urls.ts
+++ src/utils/urls.ts
@@ -1,11 +1,11 @@
-export function buildQueryString(params) {
+export function buildQueryString(params: {[key: string]: any}) {
   return Object.keys(params)
-    .map((key) => [key, params[key]])
+    .map((key) => [key, params[key]] as [string, any])
     .map((pair) => pair.map(encodeURIComponent))
     .map((pair) => pair.join('='))
     .join('&');
 }
 
-export function buildUrl(baseUrl, params) {
+export function buildUrl(baseUrl: string, params: {[key: string]: any}) {
   return `${baseUrl}?${buildQueryString(params)}`;
 }
